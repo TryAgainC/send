641d54a310e08f3164eebf847dab1415067f90f0
TryAgainC <63404307+TryAgainC@users.noreply.github.com>
2022-03-29 13:11:41 +0800

    1

---
+++ tag/13.py
@@ -0,0 +1,33 @@
+import requests
+import random
+import time
+import threading
+
+def regist():
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0'}
+    url = "https://user.taggood-5.xyz/api/v1/passport/auth/register"
+    qq_num = random.randint(1,9999999999)
+
+    qq_email = "%d@qq.com" % qq_num
+
+    qq_password = random.randint(1,999999999999)
+
+    data = {
+        "email": qq_email,
+        "password": qq_password,
+        'invite_code': 'Y5GI2Ybf'
+    }
+
+    r = requests.post(url=url,data=data,headers=headers)
+    print(qq_email,qq_password,r)
+
+
+while(True):
+    t1 = threading.Thread(target=regist())
+    t1.start()
+
+
+
+
+
+
