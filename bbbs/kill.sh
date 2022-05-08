d47ab51b351436ff45a51772bbbba09af7609751
TryAgainC <63404307+TryAgainC@users.noreply.github.com>
2022-05-08 22:56:14 +0800

    0

---
+++ bbbs/kill.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+pid=`ps -aux |grep "python3 1.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid
+
+pid2=`ps -aux |grep "python3 2.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid2
+
+pid3=`ps -aux |grep "python3 3.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid3
+
+pid4=`ps -aux |grep "python3 4.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid4
+
+pid5=`ps -aux |grep "python3 5.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid5
+
+pid6=`ps -aux |grep "python3 6.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid6
+
+pid7=`ps -aux |grep "python3 7.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid7
+
+pid8=`ps -aux |grep "python3 8.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid8
+
+pid9=`ps -aux |grep "python3 9.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid9
+
+pid10=`ps -aux |grep "python3 10.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid10
+
+pid11=`ps -aux |grep "python3 11.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid11
+
+pid12=`ps -aux |grep "python3 12.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid12
+
+pid13=`ps -aux |grep "python3 13.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid13
+
+pid14=`ps -aux |grep "python3 14.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid14
+
+pid15=`ps -aux |grep "python3 15.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid15
+
+pid16=`ps -aux |grep "python3 16.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid16
+
+pid17=`ps -aux |grep "python3 17.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid17
+
+pid18=`ps -aux |grep "python3 18.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid18
+
+pid19=`ps -aux |grep "python3 19.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid19
+
+pid20=`ps -aux |grep "python3 20.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid20
+
+pid21=`ps -aux |grep "python3 21.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid21
+pid22=`ps -aux |grep "python3 22.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid22
+pid23=`ps -aux |grep "python3 23.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid23
+pid24=`ps -aux |grep "python3 24.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid24
+pid25=`ps -aux |grep "python3 25.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid25
+pid26=`ps -aux |grep "python3 26.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid26
+pid27=`ps -aux |grep "python3 27.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid27
+pid28=`ps -aux |grep "python3 28.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid28
+pid29=`ps -aux |grep "python3 29.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid29
+pid30=`ps -aux |grep "python3 30.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid30
+pid31=`ps -aux |grep "python3 31.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid31
+pid32=`ps -aux |grep "python3 32.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid32
+pid33=`ps -aux |grep "python3 33.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid33
+pid34=`ps -aux |grep "python3 34.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid34
+pid35=`ps -aux |grep "python3 35.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid35
+pid36=`ps -aux |grep "python3 36.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid36
+pid37=`ps -aux |grep "python3 37.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid37
+pid38=`ps -aux |grep "python3 38.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid38
+pid39=`ps -aux |grep "python3 39.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid39
+pid40=`ps -aux |grep "python3 40.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid40
+pid41=`ps -aux |grep "python3 41.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid41
+pid42=`ps -aux |grep "python3 42.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid42
+pid43=`ps -aux |grep "python3 43.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid43
+pid44=`ps -aux |grep "python3 44.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid44
+pid45=`ps -aux |grep "python3 45.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid45
+pid46=`ps -aux |grep "python3 46.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid46
+pid47=`ps -aux |grep "python3 47.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid47
+pid48=`ps -aux |grep "python3 48.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid48
+pid49=`ps -aux |grep "python3 49.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid49
+pid50=`ps -aux |grep "python3 50.py"|grep -v "grep" |awk '{print $2}'`
+kill -9 $pid50
+
+
+
